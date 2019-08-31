49e74a9d78e94a482d2e9418045e3310e939adc4
wenjieyek <dx3152@hotmail.com>
2019-08-31 14:27:45 +0800

    fix bug

--- lib/coingecko_ruby/models/api_link.rb
+++ lib/coingecko_ruby/models/api_link.rb
@@ -12,23 +12,13 @@ module CoingeckoRuby
       def self.simple_url(ids, vs_currencies, include_market_cap,
                           include_24hr_vol, include_24hr_change,
                           include_last_updated_at)
-        "#{API_ENDPOINT}/simple/price?ids=#{ids}
-        &vs_currencies=#{vs_currencies}
-        &include_market_cap=#{include_market_cap}
-        &include_24hr_vol=#{include_24hr_vol}
-        &include_24hr_change=#{include_24hr_change}
-        &include_last_updated_at=#{include_last_updated_at}"
+        "#{API_ENDPOINT}/simple/price?ids=#{ids}&vs_currencies=#{vs_currencies}&include_market_cap=#{include_market_cap}&include_24hr_vol=#{include_24hr_vol}&include_24hr_change=#{include_24hr_change}&include_last_updated_at=#{include_last_updated_at}"
       end
 
       def self.coin_url(id, localization, tickers,
                         market_data, community_data, developer_data,
                         sparkline)
-        "#{API_ENDPOINT}/coins/#{id}?localization=#{localization}
-        &tickers=#{tickers}
-        &market_data=#{market_data}
-        &community_data=#{community_data}
-        &developer_data=#{developer_data}
-        &sparkline=#{sparkline}"
+        "#{API_ENDPOINT}/coins/#{id}?localization=#{localization}&tickers=#{tickers}&market_data=#{market_data}&community_data=#{community_data}&developer_data=#{developer_data}&sparkline=#{sparkline}"
       end
     end
   end
